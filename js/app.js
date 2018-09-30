3e4d146f5af1d488bf6fd9e94050a92fb3971683
Jayce Azua <jayce.azua@gmail.com>
2018-09-30 03:12:10 -0700

    in progress

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ let cookieCount = 0;
 let clickPower = 1;
 let clickPowerPriceAmount = 50;
 let clickPowerLevelNumber = 1;
+let grandmaPower = 50;
+let grandmaPriceAmount = 500;
+let grandmaLevelNumber = 0;
+let grandmaAuto = false;
 //declare DOM variables
 let cookieCounter = document.getElementById('cookie-counter');
 let cookieClicker = document.getElementById('cookie-clicker');
@@ -10,6 +14,10 @@ let buyClickPower = document.getElementById('buy-click-power');
 let clickPowerPrice = document.getElementById('click-power-price');
 let clickPowerLevel = document.getElementById('click-power-level');
 let clickPowerMultiple = document.getElementById('click-power-multiple');
+let buyGrandma = document.getElementById('buy-grandma');
+let grandmaPrice = document.getElementById('grandma-price');
+let grandmaLevel = document.getElementById('grandma-level');
+let grandmaMultiple = document.getElementById('grandma-multiple');
 
 let refreshCookieCount = () => {
   cookieCounter.innerHTML = cookieCount;
@@ -18,7 +26,22 @@ let refreshCookieCount = () => {
 let refreshPowerClick = () => {
   clickPowerLevel.innterHTML = clickPowerLevelNumber;
   clickPowerPrice.innerHTML = clickPowerPriceAmount;
+  clickPowerMultiple.innerHTML = clickPower;
 };
+
+let refreshGrandma = () => {
+  grandmaLevel.innerHTML = grandmaLevelNumber
+  grandmaPrice.innerHTML = grandmaPriceAmount;
+  grandmaMultiple.innerHTML = grandmaPower;
+  grandmaMultiple.innerHTML = grandmaPower - 10;
+};
+
+let autoGrandmaStart = () => {
+  let grandmaInt = window.setInterval(() => {
+    cookieCount += grandmaPower;
+    refreshCookieCount();
+  }, 1000);
+}
 
 cookieClicker.addEventListener('click', () => {
     cookieCount += clickPower;
@@ -34,9 +57,31 @@ buyClickPower.addEventListener("click", () => {
         clickPowerLevelNumber += 1;
         //Update click price
         clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 1.33);
+        //update Click Power
+        clickPower += 1;
         //refresh shop item
         refreshPowerClick();
     } else {
         console.log("You don't have enough cookies!");
     };
+});
+//buy a grandma
+buyGrandma.addEventListener("click", () => {
+    // Make sure we have enough cookies and subtract our cookies from the price.
+    if (cookieCount >= grandmaPriceAmount) {
+      // Subtract cookies from the price of the item.
+      cookieCount +=  - grandmaPriceAmount;
+      refreshCookieCount()
+      // Upgrade power level
+      grandmaLevelNumber += 1;
+      // Update price.
+      grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
+      // Update grandma power
+      grandmaPower += 10;
+      // Turn autoGrandma on!
+      autoGrandma = true
+      autoGrandmaStart();
+      // Refresh shop item
+      refreshGrandma();
+  };
 });
